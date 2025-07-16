cc34366a40c7c1e6ab489bc9f2b8f146816b217c
楊兒靜 <universe5961@gmail.com>
2025-07-16 14:25:35 +0800

    (fixed) a function that apply imsmooth() on FITS
    fix problem about 'rmdir : directory not empty'

--- do-smooth.py
+++ do-smooth.py
@@ -7,16 +7,19 @@
 '''
 from casatasks import importfits, imsmooth, exportfits
 import os
+import shutil
+
+shutil.rmtree('/folder_name')
 def Do_smooth(pathIN, kernel, tarBeam, fitsOUT) :
     importfits(fitsimage=pathIN, imagename='casaIN.image', overwrite=True) 
     imsmooth(imagename='casaIN.image', outfile='casa_OUT.image', kernel=kernel, beam=tarBeam, overwrite=True)
     exportfits(imagename='casa_OUT.image', fitsimage=fitsOUT, overwrite=True)
-    os.rmdir('casaIN.image')
-    os.rmdir('casaOUT.image')
+    shutil.rmtree('/casaIN.image')
+    shutil.rmtree('/casaOUT.image')
 
 
 # for example
 CO_path = '/Users/aqing/Documents/1004/Circinus_galaxy/Ratio-Map/mvpData/CO(1-0)_mom0_2.2sigma.fits' # 起到一個增加可讀性的功能？
-CO_out = 'CO22_imsmooth4.54.5'
+CO_out = 'CO22_imsmooth33'
 tarBeam = {'major': '3arcsec', 'minor': '3arcsec', 'pa': '0deg'}
 Do_smooth(CO_path, 'g', tarBeam, CO_out)
