d0e90705248c50f95d9c5d4728fd160a5dc84604
楊兒靜 <universe5961@gmail.com>
2025-07-18 08:38:34 +0800

    (modi) a function that apply imsmooth() on FITS
    將「卷積到目標解析度 (targetres=True)」設為函式預設值，
    否則是每張圖多卷一個 tarBeam
    就像沒有 deconvolve() 一樣

--- do-smooth.py
+++ do-smooth.py
@@ -10,7 +10,7 @@ import shutil
 
 def Do_smooth(pathIN, kernel, tarBeam, fitsOUT) :
     importfits(fitsimage=pathIN, imagename='casaIN.image', overwrite=True) 
-    imsmooth(imagename='casaIN.image', outfile='casaOUT.image', kernel=kernel, beam=tarBeam, overwrite=True)
+    imsmooth(imagename='casaIN.image', outfile='casaOUT.image', kernel=kernel, beam=tarBeam, targetres=True, overwrite=True)
     exportfits(imagename='casaOUT.image', fitsimage=fitsOUT, overwrite=True)
     shutil.rmtree('casaIN.image')
     shutil.rmtree('casaOUT.image')
@@ -20,5 +20,5 @@ def Do_smooth(pathIN, kernel, tarBeam, fitsOUT) :
 # for example
 CO_path = '/Users/aqing/Documents/1004/Circinus_galaxy/Ratio-Map/mvpData/CO(1-0)_mom0_2.2sigma.fits' # 起到一個增加可讀性的功能？
 CO_out = 'CO22_imsmooth33'
-tarBeam = {'major': '3arcsec', 'minor': '3arcsec', 'pa': '0deg'}
+tarBeam = {'major': '3arcsec', 'minor': '3arcsec', 'pa': '0deg'} # 預設「卷積到目標解析度(targetres)」為預設
 Do_smooth(CO_path, 'g', tarBeam, CO_out)
