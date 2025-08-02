c3278c1db35d07b4b93495e35081926f19e508c1
Livia <universe5961@gmail.com>
2025-08-03 02:07:15 +0800

    upload-add CO(1-0) in to pipeline
    把自訂函式刪掉
    流水線寫順暢（自己說）了點

--- mom0_smoothFirst/py_RMS_imstat.py
+++ mom0_smoothFirst/py_RMS_imstat.py
@@ -7,30 +7,29 @@ from casatasks import importfits, imstat
 import numpy as np
 import shutil
 
-def Stat_fits(pathIN, chans) :
-    importfits(fitsimage=pathIN, imagename='casaIN.image', overwrite=True)
-    #print('tataima!') 
-    the_stats = imstat('casaIN.image', chans=chans)
-    #print('RMS =', the_stats['rms'], 'Jy/beam')
-    #print('Use "print(the_stats[strr])" to get other stats info')
-    shutil.rmtree('casaIN.image')
-    print('All metafiles have been cleaned')
-    return the_stats
+#
+commonPath = '/Users/aqing/Documents/1004/Circinus_galaxy/Ratio-Map/mvp_smoothFirst/1_cube_smoothed/'
+parti_fileName = ['HCN-1-0', 'HCO+-1-0', 'CN-J12-12', 'CN-J32-12', 'HNC-1-0', 'CS-J2-1']
+# 因為CO 的空白裁掉了，不確定怎麼用，所以就沿用之前的估計...
+chansL = [['1063~1067', '2624~2628', '1854~1858'], # HCN(1-0)
+          ['1063~1067', '2624~2628', '1854~1858'], # HCO+(1-0)
+          ['1757~1761', '2456~2460', '3596~3600'], # CN(J1/2-1/2)
+          ['368~372', '3022~3026', '1797~1801'], # CN(J3/2-1/2)
+          ['1574~1578', '2069~2073', '3238~3242'], # HNC(1-0)
+          ['1201~1205', '2494~2499', '2626~2630'] # CS(J2-1)
+          ]
 
 # Do the cacu
-pathIN = '/Users/aqing/Documents/1004/Circinus_galaxy/Ratio-Map/mvp_smoothFirst/cube_smoothed/HCN-1-0_cube_smoothed-4545.fits'
-chansL = ['1063~1067', '2624~2628', '1854~1858'] # HCN(1-0)
-'''
-pathIN = '/Users/aqing/Documents/1004/Circinus_galaxy/Ratio-Map/mvp_smoothFirst/cube_smoothed/CN-J12-12_cube_smoothed-4545.fits'
-chansL = ['1757~1761', '2456~2460', '3596~3600'] # CN(J12-12)
+for n in range(len(parti_fileName)):
+    rms = []
+    pathIN = f'{commonPath}{parti_fileName[n]}_cube_smoothed-4545.fits'
+    importfits(fitsimage=pathIN, imagename='casaIN.image', overwrite=True)
+    for i in chansL[n] :
+        the_stats = imstat('casaIN.image', chans=i)
+        rms.append(the_stats['rms'])
+    print(rms)
+    print(f"!!! {parti_fileName[n]}'s RMS = {np.average(rms)} Jy/beam !!!")
+    shutil.rmtree('casaIN.image')
+    print('All metafiles have been cleaned', end='\n\n')
 
-pathIN = '/Users/aqing/Documents/1004/Circinus_galaxy/Ratio-Map/mvp_smoothFirst/cube_smoothed/HNC-1-0_cube_smoothed-4545.fits'
-chansL = ['1574~1578', '2069~2073', '3238~3242'] # HNC(1-0)
-'''
-rms = []
-for i in range(len(chansL)) :
-    the_stats = Stat_fits(pathIN, chansL[i])
-    rms.append(the_stats['rms'])
-print()
-print(rms)
-print(f'!!! RMS = {np.average(rms)} Jy/beam !!!')
+    print('FIN')
